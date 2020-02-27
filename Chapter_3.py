445c9b95ef1916551d4858c1b8f391d64f7659ad
= <AIK@AIK.hacettepe.edu.tr>
2020-02-27 17:35:51 +0100

    Initial commit

---
+++ Chapter_3.py
@@ -0,0 +1,13 @@
+import turtle
+
+screen = turtle.Screen()
+
+leonardo = turtle.Turtle()
+
+leonardo.forward(50)
+leonardo.left(60)
+leonardo.forward(50)
+leonardo.color("Red")
+leonardo.circle(10)
+
+screen.exitonclick()
